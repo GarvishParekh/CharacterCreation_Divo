f88f2d69c1237112f4337714053942c6e65f4f24
Garvish Parekh <garvish.p@theintellify.com>
2023-09-26 18:57:49 +0530

    Remove Everything

--- Assets/_Character Creation/_Scripts/Scriptable Scripts/PlayerPerfTags.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-
-[CreateAssetMenu(fileName = "PlayerPref Information", menuName = "PlayerPref/Tags")]
-public class PlayerPerfTags : ScriptableObject
-{
-    public string CharacterType;
-}
